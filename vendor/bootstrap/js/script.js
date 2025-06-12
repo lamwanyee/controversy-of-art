f1a1f1d8c05f8600ff6ce433d2566f8f668b2211
lamwanyee <241028@hchs.edu.my>
2025-06-12 15:36:18 +0800

    .

--- vendor/bootstrap/js/script.js
+++
@@ -1,9 +0,0 @@
-function sendMail(){
-    let parms ={
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value,
-    }
-    emailjs.send("service_hus2fec","template_u4rf28f",parms).then(alert("Email Sent!!"))
-}
