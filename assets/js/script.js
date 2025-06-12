0f8514eb95d4cb6b38a6134d44770b4d1e9f874b
lamwanyee <241028@hchs.edu.my>
2025-06-12 15:20:28 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function sendMail(){
     let parms = {
     name : document.getElementById("name").value,
     email : document.getElementById("email").value,
-    subject : document.getElementById("subject").value,
+    subject : document.getElementById("subjec").value,
     message : document.getElementById("message").value,
 }
     emailjs.send("service_hus2fec","service_hus2fec",parms).then(alert("Email Sent!!!"))
