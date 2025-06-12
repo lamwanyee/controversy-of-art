89eca1792a624c27db2bbedce8cac276030eb75e
lamwanyee <241028@hchs.edu.my>
2025-06-12 15:45:00 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,6 @@ function sendMail(){
     email : document.getElementById("email").value,
     subject : document.getElementById("subject").value,
     message : document.getElementById("message").value,
-}
+        }
     emailjs.send("service_hus2fec","template_ldq07bk",parms).then(alert("Email Sent!!!"))
-}
+    }
