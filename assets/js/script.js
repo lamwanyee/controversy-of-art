616538d8d0d72a61a6db30f83dc7539c7bb09d86
lamwanyee <241028@hchs.edu.my>
2025-06-12 15:17:12 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function sendMail(){
     let parms = {
     name : document.getElementById("name").value,
-    email : document.getElementById("name").value,
+    email : document.getElementById("email").value,
     subject : document.getElementById("subject").value,
     message : document.getElementById("message").value,
 }
