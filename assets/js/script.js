9d3a84dd359ec12adbdd4f8f9404fe50619d18db
lamwanyee <241028@hchs.edu.my>
2025-06-21 22:24:37 +0800

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
-function sendMail() {
-    // Get form values
+function sendMail(event) {
+    event.preventDefault(); // Prevents the form from submitting normally
+
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
     let subject = document.getElementById("subject").value.trim();
     let message = document.getElementById("message").value.trim();
 
-    // Check if any field is empty
     if (!name || !email || !subject || !message) {
         alert("Please fill in all fields before sending the email.");
-        return; // Stop the function
+        return;
     }
 
-    // All fields are filled; proceed to send email
     let parms = {
         name: name,
         user_email: email,
